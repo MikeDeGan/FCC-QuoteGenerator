158513b5dbb103daa6e317b0b097d82340eda7aa
Mike DeGan <mikedegan@gmail.com>
2018-12-30 14:24:33 -0500

    initial commit. grabbing quotes with Fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.fetchQuotes();
+  }
+
+  fetchQuotes() {
+    fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    )
+      .then(response => response.json())
+      .then(json => this.setState({ quotes: json.quotes, isLoading: false }))
+      .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <div id="quote-box">
+          <div id="text">This is where the quotes will go.</div>
+          <div id="author"> Author Name</div>
+          <button id="new-quote" className="btn">
+            New Quote
+          </button>
+          <a href="#" id="tweet-quote">
+            Tweet-it
           </a>
-        </header>
+        </div>
       </div>
     );
   }
