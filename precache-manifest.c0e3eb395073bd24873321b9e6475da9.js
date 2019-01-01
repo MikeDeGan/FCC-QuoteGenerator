18c8cbd6a3279b094cfbac2bc3a364c0c2baf37d
Mike DeGan <mikedegan@gmail.com>
2019-01-01 00:06:26 -0500

    Updates

---
+++ precache-manifest.c0e3eb395073bd24873321b9e6475da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a10f784a36b4e8080726",
+    "url": "/FCC-QuoteGenerator/static/js/runtime~main.a10f784a.js"
+  },
+  {
+    "revision": "5ccf5bddb8f6da90e0f8",
+    "url": "/FCC-QuoteGenerator/static/js/main.5ccf5bdd.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/FCC-QuoteGenerator/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "5ccf5bddb8f6da90e0f8",
+    "url": "/FCC-QuoteGenerator/static/css/main.748c06e6.chunk.css"
+  },
+  {
+    "revision": "a4c976dd614f103df518d2db145d604a",
+    "url": "/FCC-QuoteGenerator/index.html"
+  }
+];
